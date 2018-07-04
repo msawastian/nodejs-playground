1c705836fbdc97e7653349f4350c40a0b31cbfdb
m.sawastian <m.sawastian@gmail.com>
2018-07-04 16:17:54 +0200

    Fiddling

--- src/app.js
+++ src/app.js
@@ -1,4 +1,27 @@
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
+
+const argv = yargs.argv;
+let command = argv._[0];
+console.log(argv);
+
+if (command === 'add') {
+    let note = notes.addNote(argv.title, argv.body);
+    console.log(note);
+    if (note) {
+        console.log('Note created', argv.title, argv.body);
+    } else {
+        console.log('Failed to create note')
+    }
+} else if (command === 'list') {
+    notes.getAll();
+} else if (command === 'read') {
+    notes.getNote(argv.title)
+} else if (command === 'remove') {
+    notes.removeNote(argv.title);
+} else {
+    console.log('Command not recognized');
+}
--- src/notes.js
+++ src/notes.js
@@ -1,3 +1,50 @@
-module.exports.addNote = () => {
-    return 'New note';
+const fs = require('fs');
+
+const fetchNotes = () => {
+    try {
+        let notesString = fs.readFileSync('notes.json');
+        return JSON.parse(notesString)
+    } catch (error) {
+        return []
+    }
+};
+
+const saveNotes = notes => {
+    fs.writeFileSync('notes.json', JSON.stringify(notes));
+};
+
+const addNote = (title, body) => {
+    let note = {
+        title,
+        body
+    };
+
+    let notes = fetchNotes();
+
+    let duplicateNotes = notes.filter(note => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        saveNotes(notes);
+        return note
+    }
+};
+
+const getAll = () => {
+    console.log('Getting all notes');
+};
+
+const getNote = title => {
+    console.log(`Getting note: ${title}`)
+};
+
+const removeNote = title => {
+    console.log(`Removing note: ${title}`)
+};
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 };
