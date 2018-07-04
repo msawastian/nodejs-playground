a7e53c55ff0c1c18c8010eaaf0db9efb2ba6024f
m.sawastian <m.sawastian@gmail.com>
2018-07-04 14:11:10 +0200

    Cleanup

--- src/app.js
+++ src/app.js
@@ -1,14 +1,4 @@
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-const user = os.userInfo();
-let result = notes.add(1, 2);
-console.log(result);
-
-
-fs.appendFile('notes.txt', `Hello, ${user.username}. You are ${notes.age}!`, function (error) {
-    if (error) {
-        console.log(error);
-    }
-});
