b101d9ffa5d7a1c3eb77bcfbacfd88af1428e652
m.sawastian <m.sawastian@gmail.com>
2018-07-05 17:04:20 +0200

    Weather fiddling

--- src/weather/app.js
+++ src/weather/app.js
@@ -1,6 +1,6 @@
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
-const request = require('request');
+const weather = require('./weather');
 
 const argv = yargs
     .options({
@@ -19,18 +19,14 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
     if (errorMessage) {
         console.log(errorMessage)
     } else {
-        console.log(JSON.stringify(results, undefined, 2))
-    }
-});
-
-request({
-    url: `https://api.darksky.net/forecast/95c7f7665261b2550dc0a64c3cc9ab09/52.4293162,16.9221588`,
-    json: true
-}, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-        console.log(body.currently.temperature)
-    } else {
-        console.log('Unable to fetch weather');
+        console.log(results.address);
+        weather.getWeather(results.latitude, results.longitude, (errorMessage, results) => {
+            if (errorMessage) {
+                console.log(errorMessage)
+            } else {
+                console.log(`It's currently ${results.temperature}. It feels like ${results.apparentTemperature}`)
+            }
+        });
     }
 });
 
