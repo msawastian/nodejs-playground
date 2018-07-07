d1385ba4873dc86764c0873a2606658defa21ac7
m.sawastian <m.sawastian@gmail.com>
2018-07-07 21:12:56 +0200

    Heroku

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use( (request, response, next) => {
+    const now = new Date().toString();
+    const log = `${now}: ${request.method} ${request.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', err => {
+        if (err) {
+            console.log('Unable to append to server.log')
+        }
+    });
+    next();
+});
+
+app.use( (request, response, next) => {
+    response.render('maintenance.hbs');
+});
+
 app.use(express.static('public'));
+
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+    return text.toUpperCase();
 });
 
 app.get('/', (request, response) => {
@@ -30,4 +55,4 @@ app.get('/bad', (request, response) => {
     })
 });
 
-app.listen(3000, () => console.log('Server is up on port 3000'));
+app.listen(port, () => console.log(`Server is up on port ${port}`));
