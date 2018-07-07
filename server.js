53c90385fb1b7f580b43a4133be876264b056b33
m.sawastian <m.sawastian@gmail.com>
2018-07-07 12:08:57 +0200

    Web server

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
 app.get('/', (request, response) => {
-    // response.send('<h1>Hello Express!</h1>');
-    response.send({
-        name: 'Mat',
-        likes: [
-            'JS',
-            'CSS'
-        ]
-    })
+    response.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Hello there!'
+    });
 });
 
 app.get('/about', (request, response) => {
-    response.send('About page');
+   response.render('about.hbs', {
+       pageTitle: 'About Page'
+   });
 });
 
 app.get('/bad', (request, response) => {
